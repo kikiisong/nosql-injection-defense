ad5a9cac2991fdcd5de6565af22c0c3093e9d866
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-12-02 15:31:20 -0500

    trained

--- server/index.js
+++ server/index.js
@@ -10,6 +10,39 @@ app.use(bodyParser.json());
 // for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+var natural = require('natural');
+var fs = require('fs');
+
+var data = fs.readFileSync('sqli_v4.csv')
+    .toString() // convert Buffer to string
+    .split('\n') // split string to lines
+    .map(e => e.trim()) // remove white spaces for each line
+    .map(e => e.split('::::').map(e => e.trim())); // split each line to array
+
+//data = data.copyOf(data, data.length-2);
+console.log(data.length);
+
+for (item in data){
+	if (item < data.length){
+		var val = data[item][1].charAt(0);
+		data[item][1] = val;
+	}
+
+	else {
+		console.log('done');
+		break;
+	}
+}
+
+var nb = new natural.BayesClassifier();
+//const [train, test] = trainTestSplit(data, 0.8);
+
+for (i in data){
+	nb.addDocument(data[i][0], data[i][1]);
+}
+
+nb.train();
+
 // Replace the uri string with your connection string.
 // you might want to follow https://www.mongodb.com/docs/drivers/node/current/quick-start/
 // to set up your own database or let me (Karine) know we can figure it out together
@@ -68,6 +101,13 @@ async function checkLogin(user_input) {
     const database = client.db('testing');
     const testData = database.collection('users');
     
+    const classfied = nb.classify(JSON.stringify(user_input));
+    if(parseInt(classfied)) 
+    {
+      console.log(user_input);
+      console.log("malicious");
+      return false;
+    }
     const query = { username: user_input.username, password: user_input.password };
     const cursor = testData.find(query);
     const allValues = await cursor.toArray();
